7da6a42aa4ea267cad71f5834f72c99f792f327d
Etienne Bellé <bl.etienne7@gmail.com>
2024-10-29 15:52:55 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+
+dotenv.config();
+
+export default defineConfig({
+  publicDir: 'resources/static',
+  build: {
+    assetsDir: '',
+    emptyOutDir: true,
+    manifest: true,
+    outDir: `public/themes/${process.env.WP_DEFAULT_THEME}/assets`,
+    rollupOptions: {
+      input: [
+        'resources/js/index.js',
+        'resources/sass/index.scss'
+      ]
+    },
+  },
+  plugins: [
+    {
+      name: 'php',
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith('.php')) {
+          server.ws.send({ type: 'full-reload', path: '*' });
+        }
+      },
+    },
+  ],
+});
